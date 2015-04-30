2e758e1172062bbf591abbcd256d8f1e0155adf4
unknown <cyacarini@LP-SOFT1.GRUPORCP.PE>
2015-04-30 18:11:06 -0500

    first example commit

---
+++ prueba.py
@@ -0,0 +1,9 @@
+from dateutil import dateutil
+
+
+def on_post(req, resp):
+    if req.method == 'POST':
+        print "Something to do on POST"
+        print dateutil.dateutil.now()
+    else:
+        print "something to do on NO POST"
